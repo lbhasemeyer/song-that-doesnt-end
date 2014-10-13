e3e5133a746a85d14648bf3d6f75a1a920c99140
lbhasemeyer <lbhasemeyer@gmail.com>
2014-10-13 13:29:36 -0600

    Song that never ends - complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get "/line2" => "pages#line2", as: :line2
+  get "/line3" => "pages#line3", as: :line3
+  get "/line4" => "pages#line4", as: :line4
 end
